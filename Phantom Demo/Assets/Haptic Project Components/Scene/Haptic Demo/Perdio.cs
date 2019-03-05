b234acf0df89f4230e0917e21e09e43b9f6fef4c
babohorquez10 <ba.bohorquez10@uniandes.edu.co>
2019-03-05 18:32:02 -0500

    Demo háptico Phantom Omni

---
+++ Phantom Demo/Assets/Haptic Project Components/Scene/Haptic Demo/Perdio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perdio : MonoBehaviour {
+
+    public GameObject textoPerder;
+    private bool hayColision = false;
+    private bool reiniciar = false;
+    public GameObject textRetry;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (reiniciar && Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("Haptic Demo");
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Block"))
+        {
+            Debug.Log("Colision!!");
+            textoPerder.SetActive(true);
+            textRetry.SetActive(true);
+            reiniciar = true;
+            hayColision = true;
+        }
+    }
+
+    public bool HayColision()
+    {
+        return hayColision;
+    }
+
+}
